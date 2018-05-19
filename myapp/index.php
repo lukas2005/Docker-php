f976bd7501a4d92e10a1f0c6aa3f325bee9f9590
Krystian Wybranowski <Krystian@MacBook-Pro-Krystian.local>
2018-05-19 09:12:24 +0200

    Add style css

--- myapp/index.php
+++ myapp/index.php
@@ -1,3 +1,31 @@
+<!doctype html>
+<html lang="pl">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Krystian wybranowski">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Zadania z matematyki</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+
+    <link href="style.css" rel="stylesheet">
 <?php
-echo "Hello World!";
+$sms = "Wszystko działa poprawnie! 😍😁";
+$alerttype = "alert alert-success";
 ?>
+  </head>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  <body class="text-center">
+    <form class="form-signin">
+
+	  <div class="<?php echo $alerttype; ?>" role="alert"><?php echo $sms; ?></div>
+
+      <p class="mt-5 mb-3 text-muted">&copy; Krystian Wybranowski</p>
+    </form>
+
+ </body>
+</html>
